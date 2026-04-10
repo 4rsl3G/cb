ce6d61585f7a5ba5a4e87fe2827c142cde4dc278
Jhody Pedia <53709496+4rsl3G@users.noreply.github.com>
2026-04-10 17:30:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-require(“dotenv”).config();
-const logger = require(”./utils/logger”);
-const { startBot } = require(”./telegram/bot”);
-const { ensureDir } = require(”./utils/file”);
-const config = require(”./config”);
+require("dotenv").config();
+const logger = require("./utils/logger");
+const { startBot } = require("./telegram/bot");
+const { ensureDir } = require("./utils/file");
+const config = require("./config");
 
 async function main() {
-logger.info(“Starting WA Bulk Checker…”);
+  logger.info("Starting WA Bulk Checker...");
 
-// Ensure required directories exist
-ensureDir(config.whatsapp.sessionDir);
-ensureDir(config.output.dir);
+  // Ensure required directories exist
+  // Note: Add 'await' here if ensureDir is an asynchronous function
+  ensureDir(config.whatsapp.sessionDir);
+  ensureDir(config.output.dir);
 
-// Start Telegram bot
-try {
-startBot();
-logger.info(“System ready. Use /start in Telegram to begin.”);
-} catch (err) {
-logger.error(“Failed to start Telegram bot:”, err.message);
-process.exit(1);
-}
+  // Start Telegram bot
+  try {
+    // Note: Add 'await' here if startBot returns a Promise
+    startBot(); 
+    logger.info("System ready. Use /start in Telegram to begin.");
+  } catch (err) {
+    logger.error("Failed to start Telegram bot:", err.message);
+    process.exit(1);
+  }
 
-// Graceful shutdown
-process.on(“SIGINT”, () => {
-logger.info(“Shutting down…”);
-process.exit(0);
-});
+  // Graceful shutdown
+  process.on("SIGINT", () => {
+    logger.info("Shutting down... (Add logic here to close bot/DB connections)");
+    process.exit(0);
+  });
 
-process.on(“uncaughtException”, (err) => {
-logger.error(“Uncaught exception:”, err.message);
-});
+  process.on("uncaughtException", (err) => {
+    logger.error("Uncaught exception:", err.message);
+    // It's usually best practice to restart the process after an uncaught exception
+    process.exit(1); 
+  });
 
-process.on(“unhandledRejection”, (reason) => {
-logger.error(“Unhandled rejection:”, reason?.message || reason);
-});
+  process.on("unhandledRejection", (reason) => {
+    logger.error("Unhandled rejection:", reason?.message || reason);
+  });
 }
 
 main();
