1de107e150a9cd196bf8372b62e412948f5a8022
Jhody Pedia <53709496+4rsl3G@users.noreply.github.com>
2026-04-10 17:32:43 +0700

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,16 +1,22 @@
-const pino = require(“pino”);
-const config = require(”../config”);
+const pino = require("pino");
+const config = require("../config");
+
+// Check if we are in a development environment
+const isDev = process.env.NODE_ENV !== "production";
 
 const logger = pino({
-level: config.log.level,
-transport: {
-target: “pino-pretty”,
-options: {
-colorize: true,
-translateTime: “SYS:HH:MM:ss”,
-ignore: “pid,hostname”,
-},
-},
+  level: config.log.level,
+  // Only apply pino-pretty if we are NOT in production
+  ...(isDev && {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "SYS:HH:MM:ss",
+        ignore: "pid,hostname",
+      },
+    },
+  }),
 });
 
 module.exports = logger;
